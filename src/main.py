7cf7d835040de43706147f7a6380d2d07da7c0e5
apande <ani.pande1995@gmail.com>
2021-11-03 14:19:43 -0400

    export functionality
    csv exports now supported

--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ from os import scandir
 from typing import Optional
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.responses import FileResponse
 import json 
+import csv
 import logging
 
 # local imports
@@ -30,7 +32,7 @@ async def read_root():
     return response
 
 @app.get("/{site}/{item_name}")
-async def search_items_API(site : str,item_name: str, relevant: Optional[str] = None, order_by_col: Optional[str] = None, reverse: Optional[bool] = False, ListLengthInd: Optional[int] = 10): 
+async def search_items_API(site : str,item_name: str, relevant: Optional[str] = None, order_by_col: Optional[str] = None, reverse: Optional[bool] = False, listLengthInd: Optional[int] = 10, export: Optional[bool] = False): 
     '''Wrapper API to fetch AMAZON, WALMART and TARGET query results
 
     Parameters
@@ -51,7 +53,7 @@ async def search_items_API(site : str,item_name: str, relevant: Optional[str] = 
     'search': item_name,
     'sort' : 'pr' if order_by_col == 'price' else 'pr', # placeholder TDB
     'des' : reverse, # placeholder TBD
-    'num' : ListLengthInd,
+    'num' : listLengthInd,
     'relevant' : relevant
     }
 
@@ -71,10 +73,17 @@ async def search_items_API(site : str,item_name: str, relevant: Optional[str] = 
     #calling scraper.scrape to fetch results
     itemList =  scr.scrape(args = args, scrapers= scrapers)
  
-    #returning JSON list
-    itemListJson = json.dumps(itemList) 
-    file.close()    
-    return itemListJson
+    if not export:
+        #returning JSON list
+        itemListJson = json.dumps(itemList) 
+        file.close()    
+        return itemListJson
+    else:
+        with open('slash.csv','w', encoding='utf8', newline='') as f:
+            dict_writer = csv.DictWriter(f, itemList[0].keys())
+            dict_writer.writeheader()
+            dict_writer.writerows(itemList)
+        return FileResponse('slash.csv',media_type='application/octet-stream',filename='slash_'+item_name+'.csv')
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
