3e1f13fbc785dc1000dfad54cd8dcdff862b47a7
Rahul Kalita <rahulkalita8@gmail.com>
2021-11-23 23:00:24 -0500

    Adding default homepage

---
+++ client/src/components/Home.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275
+    },
+    pos: {
+        marginBottom: 12
+    }
+});
+
+export default function Home() {
+    const classes = useStyles();
+
+    return (
+        <Grid container>
+            <Grid item md={6}>
+                <Card className={classes.root}>
+                    <CardContent>
+                        <Typography variant="h5" component="h2">
+                            SLASH
+                        </Typography>
+                        <Typography
+                            className={classes.pos}
+                            color="textSecondary"
+                        >
+                            SE 2021
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
+    );
+}
---
+++ client/src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+
+function Menu() {
+    return (
+        <Grid container item>
+            Menu2
+        </Grid>
+    );
+}
+
+export default Menu;
