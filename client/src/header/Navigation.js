9edb1ae8fe2e6e6d98e0bc1e33e5cf11eac09fba
Rahul Kalita <rahulkalita8@gmail.com>
2021-11-23 22:59:11 -0500

    Adding navigation drawer and header

---
+++ client/src/header/Navigation.js
@@ -0,0 +1,46 @@
+import React, { useContext } from "react";
+import { makeStyles } from "@mui/styles";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { AppContext } from "../header/AppContext";
+import NavigationDrawer from "./NavigationDrawer";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        width: "100%"
+    },
+    title: {
+        flexGrow: 1
+    }
+}));
+
+export default function Navigation() {
+    const classes = useStyles();
+    const context = useContext(AppContext);
+
+    return (
+        <div className={classes.root}>
+            <NavigationDrawer />
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={context.toggleDrawer}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        SLASH
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
---
+++ client/src/header/NavigationDrawer.js
@@ -0,0 +1,45 @@
+import React, { useContext } from "react";
+
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import withStyles from "@mui/styles/withStyles";
+
+import { routes } from "../configs/routes";
+import { AppContext } from "./AppContext";
+
+import { NavLink as Link } from "react-router-dom";
+
+const styles = () => ({
+  root: {
+    width: "150",
+  },
+});
+function NavigationDrawer({ classes }) {
+  const context = useContext(AppContext);
+  return(
+    <React.Fragment>
+      <Drawer open={context.isDrawerOpen} onClose={context.toggleDrawer}>
+        <List component="nav">
+          {routes
+            .filter((route) => route.name)
+            .filter((route) => {
+              return true;
+            })
+            .map((route, index) => (
+              <Link to={route.path} key={index}>
+                <ListItem button divider className={classes.root}>
+                  <ListItemIcon>{route.icon}</ListItemIcon>
+                  <ListItemText primary={route.name} />
+                </ListItem>
+              </Link>
+            ))}
+        </List>
+      </Drawer>
+    </React.Fragment>
+  );
+}
+
+export default withStyles(styles)(NavigationDrawer);
