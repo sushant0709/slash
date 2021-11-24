24dc0e12e5f79dee0c8af24f663cc69f907fd6ab
Rahul Kalita <rahulkalita8@gmail.com>
2021-11-23 23:00:50 -0500

    Adding Main page for the frontend

---
+++ client/src/Main.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Routing from "./routes/Routing";
+import Grid from "@mui/material/Grid";
+import Navigation from "./header/Navigation";
+import AppContainer from "./header/AppContainer";
+
+function Main() {
+  return (
+      <AppContainer>
+        <Grid container direction="row">
+          <Grid item md={12}>
+            <Navigation />
+          </Grid>
+          <Grid item lg={12}>
+            <Routing />
+          </Grid>
+        </Grid>
+      </AppContainer>
+  );
+}
+
+export default Main;
