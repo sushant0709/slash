99482090b5cf5644975395fefd1913e74457f169
kirantejatummuri <ktummur@ncsu.edu>
2021-11-28 14:55:46 -0500

    Added chart visualization of analysis of a search item

--- client/src/components/Charts.js
+++ client/src/components/Charts.js
@@ -2,44 +2,22 @@ import React, { useEffect, useState } from 'react';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement,
   LineElement, Title, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie, Line } from 'react-chartjs-2';
-import faker from 'faker';
+import Button from '@mui/material/Button';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { Stack, TextField } from '@mui/material';
+
 
 ChartJS.register(CategoryScale, LinearScale, PointElement,
   LineElement, Title, ArcElement, Tooltip, Legend);
 
-// const data = {
-//   labels: ['Amazon', 'Walmart', 'Target', 'Costco', 'BestBuy', 'ebay'],
-//   datasets: [
-//     {
-//       label: 'Variety of collections available',
-//       data: [60, 0, 26, 24, 24, 31],
-//       backgroundColor: [
-//         'rgba(255, 99, 132, 0.2)',
-//         'rgba(54, 162, 235, 0.2)',
-//         'rgba(255, 206, 86, 0.2)',
-//         'rgba(75, 192, 192, 0.2)',
-//         'rgba(153, 102, 255, 0.2)',
-//         'rgba(255, 159, 64, 0.2)',
-//       ],
-//       borderColor: [
-//         'rgba(255, 99, 132, 1)',
-//         'rgba(54, 162, 235, 1)',
-//         'rgba(255, 206, 86, 1)',
-//         'rgba(75, 192, 192, 1)',
-//         'rgba(153, 102, 255, 1)',
-//         'rgba(255, 159, 64, 1)',
-//       ],
-//       borderWidth: 1,
-//     },
-//   ],
-// };
-
 function Charts() {
-  useEffect(() => {
+
+  const handleSubmission = () => {
+    setButton('Fetching')
     const fetchVariety = async () => {
-      const res = await fetch("http://localhost:8000/analysis/varietyCount/all/chair")
+      const res = await fetch('http://localhost:8000/analysis/varietyCount/all/' + searchItem)
       const data = await res.json()
-      setChartData({
+      setPieData({
         labels: data.map((variety) => variety.website),
         datasets: [
           {
@@ -65,24 +43,115 @@ function Charts() {
           }
         ]
       });
+      setButton('Fetched')
+    };
+    const fetchCosts = async () => {
+      const res = await fetch('http://localhost:8000/analysis/topCost/all/' + searchItem)
+      const data = await res.json()
+      setLineData({
+        labels: data.map((costs) => costs.website),
+        datasets: [
+          {
+            label: 'Lowest price on each website',
+            data: data.map((costs) => costs.lowest_price),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ]
+          },
+          {
+            label: 'Highest price on each website',
+            data: data.map((costs) => costs.highest_price),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ]
+          }
+        ]
+      });
+      setButton('Fetched')
     };
     fetchVariety()
-  }, []);
+    fetchCosts()
+  };
 
-  var [chartData, setChartData] = useState(undefined)
+  const [pieData, setPieData] = useState(undefined)
+  const [lineData, setLineData] = useState(undefined)
+  const [button, setButton] = useState('Default')
+  const [searchItem, setSearchItem] = useState(undefined)
+  
 
     return (
 
-      <div style={{display:"flex", width:"60vw", height:"80vh", marginBottom:"15vh", marginTop:"5vh"}}>
-      { chartData != undefined ? (
-        <div>
-      <h2 style={{margin: "auto", marginLeft:"3vw", marginRight:"3vw"}}> Variety of collections available across websites </h2>
+      <div>
+        <div style={{display:'flex', marginLeft:"1vw", marginTop:'2vh'}}>
+        {button === 'Default' ? (
+          <Stack direction='row' alignItems='center' spacing={2}>
+          <TextField id='outlined-basic' label='Search Item' variant='outlined' 
+          onChange={(e) => setSearchItem(e.target.value)} />
+          <Button size='medium' variant='contained' color='secondary' onClick={handleSubmission}>
+            Chart Results
+          </Button>
+          </Stack>
+          ) 
+          : button === 'Fetching' ? (
+            <LoadingButton loading variant='outlined'>
+              Submit
+            </LoadingButton>)
+          : <div></div>}
+        </div>
+      { pieData != undefined ? (
+        <div style={{display:'flex', width:'60vw', height:'80vh', marginBottom:'15vh', marginTop:'5vh'}}>
+      <h2 style={{margin: 'auto', marginLeft:'3vw', marginRight:'3vw', marginBottom:'2vh'}}> Variety of {searchItem} available across websites </h2>
           <Pie 
-          data={chartData}
+          data={pieData}
           options={{
             title:{
               display:true,
               text:'Variety of collections available',
+              fontSize:20
+            },
+            responsive: true,
+            maintainAspectRatio: false
+          }}
+          />
+          </div>
+      ) : (
+        <div></div>
+      )}
+      { lineData != undefined ? (
+        <div style={{display:'flex', width:'60vw', height:'80vh', marginBottom:'15vh', marginTop:'5vh'}}>
+      <h2 style={{margin: 'auto', marginLeft:'3vw', marginRight:'3vw', marginBottom:'2vh'}}> Highest and lowest prices of {searchItem} on each website </h2>
+          <Line 
+          data={lineData}
+          options={{
+            title:{
+              display:true,
+              text:'Highest and lowest prices on each website',
               fontSize:20
             },
             responsive: true,
