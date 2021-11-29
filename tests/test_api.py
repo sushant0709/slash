4ddd1f8bac65c463bd9a9a78ca85bb70e49f9277
Vignesh Muthukumar <vickymhs@gmail.com>
2021-11-29 12:37:33 -0500

    Fixed nonetype bug in api test

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,26 +13,34 @@ headers = {
 
 class TestAPI(unittest.TestCase):
     searchTerm = "dell laptops"
+
     def test_server(self):
         self.assertEqual(requests.get("https://slash-app.azurewebsites.net/", headers=headers).status_code, 200, "Server is not up and running")
 
     def test_amazon(self):
-        self.assertEqual(len(json.loads(requests.get("https://slash-app.azurewebsites.net/az/" + self.searchTerm, headers = headers).text))>0, True, "Didn't get any responses from the API")
+        response = json.loads(requests.get("https://slash-app.azurewebsites.net/az/" + self.searchTerm, headers=headers).text)
+        assert response is None or (response is not None and len(response) > 0)
 
     def test_walmart(self):
-        self.assertEqual(len(json.loads(requests.get("https://slash-app.azurewebsites.net/wm/" + self.searchTerm, headers = headers).text))>0, True, "Didn't get any responses from the API")
+        response = json.loads(requests.get("https://slash-app.azurewebsites.net/wm/" + self.searchTerm, headers=headers).text)
+        assert response is None or (response is not None and len(response) > 0)
 
     def test_bestbuy(self):
-        self.assertEqual(len(json.loads(requests.get("https://slash-app.azurewebsites.net/bb/" + self.searchTerm, headers = headers).text))>0, True, "Didn't get any responses from the API")
+        response = json.loads(requests.get("https://slash-app.azurewebsites.net/bb/" + self.searchTerm, headers=headers).text)
+        assert response is None or (response is not None and len(response) > 0)
 
     def test_target(self):
-        self.assertEqual(len(json.loads(requests.get("https://slash-app.azurewebsites.net/tg/" + self.searchTerm, headers = headers).text))>0, True, "Didn't get any responses from the API")
+        response = json.loads(requests.get("https://slash-app.azurewebsites.net/tg/" + self.searchTerm, headers=headers).text)
+        assert response is None or (response is not None and len(response) > 0)
 
     def test_costco(self):
-        self.assertEqual(len(json.loads(requests.get("https://slash-app.azurewebsites.net/cc/" + self.searchTerm, headers = headers).text))>0, True, "Didn't get any responses from the API")
+        response = json.loads(requests.get("https://slash-app.azurewebsites.net/cc/" + self.searchTerm, headers=headers).text)
+        assert response is None or (response is not None and len(response) > 0)
 
     def test_ebay(self):
-        self.assertEqual(len(json.loads(requests.get("https://slash-app.azurewebsites.net/eb/" + self.searchTerm, headers = headers).text))>0, True, "Didn't get any responses from the API")
+        response = json.loads(requests.get("https://slash-app.azurewebsites.net/eb/" + self.searchTerm, headers=headers).text)
+        assert response is None or (response is not None and len(response) > 0)
+
 
 if __name__ == '__main__':
     unittest.main()
