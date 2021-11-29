eaa713e170c9d69b4d41182d850d5b3052a7394e
Rahul Kalita <rahulkalita8@gmail.com>
2021-11-29 17:13:02 -0500

    Adding routing to results

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,44 +7,46 @@ import Grid from "@mui/material/Grid";
 import getResults from "../util";
 import Button from "@mui/material/Button";
 
+import { useNavigate } from "react-router-dom";
+
 const useStyles = makeStyles({
-    root: {
-        minWidth: 275
-    },
-    pos: {
-        marginBottom: 12
-    }
+  root: {
+    minWidth: 275
+  },
+  pos: {
+    marginBottom: 12
+  }
 });
 
 export default function Home() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    async function getResponse() {
-        let response = null
-        response = await getResults('all', 'dell')
-        // props.getResult(response);
-    }
+  const navigate = useNavigate();
 
-    return (
-        <Grid container>
-            <Grid item md={6}>
-                <Card className={classes.root}>
-                    <CardContent>
-                        <Typography variant="h5" component="h2">
-                            SLASH
-                        </Typography>
-                        <Typography
-                            className={classes.pos}
-                            color="textSecondary"
-                        >
-                            SE 2021
-                        </Typography>
-                        <Button size="medium" variant="contained" color="primary" onClick={getResponse} to="/results">
-                            Get Results
-                        </Button>
-                    </CardContent>
-                </Card>
-            </Grid>
-        </Grid>
-    );
+  async function getResponse() {
+    let response = null;
+    console.log("Running");
+    response = await getResults("all", "dell");
+    navigate("/results", { state: { response: response } });
+  }
+
+  return (
+    <Grid container>
+      <Grid item md={6}>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              SLASH
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              SE 2021
+            </Typography>
+            <Button size="medium" variant="contained" color="primary" onClick={getResponse} to="/results">
+              Get Results
+            </Button>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 }
