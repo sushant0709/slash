334544d0c922d6c8e85260ee0f0f984a0c92ad32
Vignesh Muthukumar <vickymhs@gmail.com>
2021-11-29 12:49:49 -0500

    Added variety count test for main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,8 @@
 from os import link
 import unittest
-from src.main import getFloatPrice, getItemInfoByItemName
+
+from requests.models import Response
+from src.main import getFloatPrice, getItemInfoByItemName, getVarietyCountByWebsite
 
 
 class TestMain(unittest.TestCase):
@@ -34,14 +36,23 @@ class TestMain(unittest.TestCase):
             {'website': 'costco'},
             {'website': 'bestbuy'},
             {'website': 'ebay'}]
-        
-        itemList = getItemInfoByItemName(args)
-        assert itemList[0]['website'] == response[0]['website']
 
-    # def test_getVarietyCountByWebsite(self):
-    #     """
-    #     Assert variety count values
-    #     """
-    #     assert getFloatPrice("price of the quantity is $200.384") == 200.384
-    #     assert getFloatPrice("$10.0$") == 10.0
-    #     assert getFloatPrice("") == 0
+        itemList = getItemInfoByItemName(args)
+        assert itemList[0]['website'] == response[0]['website'] and itemList[1]['website'] == response[1]['website'] and itemList[2]['website'] == response[2]['website'] and itemList[3]['website'] == response[3]['website'] and itemList[4]['website'] == response[4]['website'] and itemList[5]['website'] == response[5]['website']
+
+    def test_getVarietyCountByWebsite(self):
+        """
+        Assert variety count values
+        """
+        itemList = [
+            {'timestamp': '29/11/2021 11:46:26', 'title': 'HOMEFUN Ergonomic Office Chair, High Bac...', 'price': '$97.98', 'link': 'www.amazon.com/HOMEFUN-Ergonomic-Adjustable-Armrests-Support/dp/B084WWQB5B/ref=sr_1_1?keywords=chair&qid=1638204385&sr=8-1', 'website': 'amazon'}, 
+            {'timestamp': '29/11/2021 11:46:28', 'title': 'Big Joe Milano Bean Bag Chair, Multiple ...', 'price': 'From $48.13', 'link': 'www.walmart.comhttps://wrd.walmart.com/track?pltfm=desktop&tax=4044_103150_97116_91853&rdf=1&plmt=sp-search-middle~desktop~&eventST=click&spQs=apqgqDEG00Gk_QH7Y98s-tvzD2GJBtYRHuyrCs-NV8UWvHysqbYcLsclYVXrTItsxT99dj59OB3kUD_JhC3RyHy-pCq4J6WjLiQcT_qrqQA&pos=1&pgId=chair&bt=1&adUid=ZavTQ7x_M1eGlAtdVKJy6__X2-ZoZz6XZJJn&storeId=1372&mloc=sp-search-middle&pt=search&rd=https%3A%2F%2Fwww.walmart.com%2Fip%2FBig-Joe-Milano-Bean-Bag-Chair-Blue%2F51744431%3Fathbdg%3DL1800', 'website': 'walmart'}, 
+            {'timestamp': '29/11/2021 11:46:29', 'title': 'Steel Folding Chair Black - PDG', 'price': '$11.99', 'website': 'target', 'link': 'https://www.target.com/p/steel-folding-chair-black-pdg/-/A-14777780'}, 
+            {'timestamp': '29/11/2021 11:46:30', 'title': 'Respawn Gaming Chair', 'price': '$229.99', 'link': 'https://www.costco.com/respawn-gaming-chair.product.100388800.html', 'website': 'costco'}, 
+            {'timestamp': '29/11/2021 11:46:35', 'title': 'Razer - Iskur Gaming Chair with Built-in...', 'price': '$349.99', 'link': 'www.bestbuy.com/site/razer-iskur-gaming-chair-with-built-in-lumbar-support-black/6476898.p?skuId=6476898', 'website': 'bestbuy'}, 
+            {'timestamp': '29/11/2021 11:46:36', 'title': 'Steelcase Leap V2 Chair,  -Open Box- Ful...', 'price': '$329.11', 'website': 'ebay', 'link': 'https://www.ebay.com/itm/Steelcase-Leap-V2-Chair-Open-Box-Fully-Loaded-Black-Fabric-/121882349228'}
+            ]
+        answer = {'amazon': 1, 'walmart': 1, 'target': 1, 'costco': 1, 'bestbuy': 1, 'ebay': 1}
+
+        response = getVarietyCountByWebsite(itemList)
+        assert response['amazon'] == answer['amazon'] and response['walmart'] == answer['walmart'] and response['target'] == answer['target'] and response['costco'] == answer['costco'] and response['bestbuy'] == answer['bestbuy'] and response['ebay'] == answer['ebay']
