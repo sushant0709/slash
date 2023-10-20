094cee940057fef5f77e0c361557e1781fa374e6
Nisarg Nilesh Doshi <70905787+Nisarg20@users.noreply.github.com>
2023-10-19 22:33:18 -0400

    Added Web Scraping and API Files.

---
+++ src/scraper/configs.py
@@ -0,0 +1,263 @@
+# package imports
+from datetime import datetime
+import requests
+from ebaysdk.finding import Connection
+
+# local imports
+from scraper.formattr import formatTitle
+
+
+COSTCO = {
+    'site': 'costco',
+    'url': 'https://www.costco.com/CatalogSearch?dept=All&keyword=',
+    'item_component': 'div',
+    'item_indicator': {
+        'class': 'product-tile-set'
+    },
+    'title_indicator': 'span a',
+    'price_indicator': 'div.price',
+    'link_indicator': 'span.description a',
+    'image_indicator': 'div.product-img-holder a.product-image-url img.img-responsive',
+    'rating_indicator': 'div.c-ratings-reviews p.visually-hidden'
+}
+
+BESTBUY = {
+    'site': 'bestbuy',
+    'url': 'https://www.bestbuy.com/site/searchpage.jsp?st=',
+    'item_component': 'li',
+    'item_indicator': {
+        'class': 'sku-item',
+    },
+    'title_indicator': 'h4.sku-title a',
+    'title': 'div.sku-title h4.sku-header a',
+    'price_indicator': 'div.pricing-price div.priceView-hero-price span',
+    'link_indicator': 'a.image-link',
+    'image_indicator': 'a.image-link img',
+    'rating_indicator': 'div.c-ratings-reviews p.visually-hidden'
+}
+# idividual scrapper
+def scrape_amazon(query):
+    """Scrape Amazon's api for data
+
+    https://www.rainforestapi.com/docs/product-data-api/overview
+
+    Parameters
+    ----------
+    query: str
+        Item to look for in the api
+
+    Returns
+    ----------
+    items: list
+        List of items from the dict
+    """
+
+    api_url = 'https://api.rainforestapi.com/request'
+
+    page = '/s/' + query
+
+    params = {
+    'api_key': 'ED96ABDD2E0145E1A2E2ABFCEF0A894E',
+    'type': 'search',
+    'amazon_domain': 'amazon.com',
+    'search_term': query,
+    'sort_by': 'price_high_to_low'
+    }
+    
+    data = requests.get(api_url, params=params).json()
+
+    items = []
+    for p in data['search_results']:
+        if 'price' in p and 'rating' in p:
+            item = {
+                'timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+                'title': formatTitle(p['title']),  
+                'price': '$' + str(p['price']['value']),
+                'website': 'amazon',
+                'link': p['link'],
+                'image': p['image'],
+                'rating':str(p['rating'])
+            }
+            items.append(item)
+        
+    return items
+
+# individual scrapers
+def scrape_walmart(query):
+    """Scrape Walmarts's api for data
+
+    https://app.bluecartapi.com/playground
+
+    Parameters
+    ----------
+    query: str
+        Item to look for in the api
+
+    Returns
+    ----------
+    items: list
+        List of items from the dict
+    """
+
+    api_url = 'https://api.bluecartapi.com/request'
+
+    page = '/s/' + query
+
+    params = {
+        'api_key': '924E6940486740DAB0E735363D8ED199',
+        'search_term': query,
+        'type': 'search'
+    }
+    
+
+    data = requests.get(api_url, params=params).json()
+
+    items = []
+    for p in data['search_results']:
+        if 'rating' in p['product']:
+            item = {
+                'timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+                'title': formatTitle(p['product']['title']),
+                'price': '$' + str(p['offers']['primary']['price']),
+                'website': 'walmart',
+                'link': p['product']['link'],
+                'image': p['product']['main_image'],
+                'rating': str(p['product']['rating'])
+            }
+        items.append(item)
+        
+    return items
+
+# individual scrapers
+def scrape_target(query):
+    """Scrape Target's api for data
+
+    https://www.redcircleapi.com/docs/target-product-data-api/overview
+    https://api.redcircleapi.com/request?api_key=35C450DAD7CB44A894C1DA0B6A62C7A6&search_term=highlighter+pens&category_id=5zja3&type=search
+
+    Parameters
+    ----------
+    query: str
+        Item to look for in the api
+
+    Returns
+    ----------
+    items: list
+        List of items from the dict
+    """
+
+    api_url = 'https://api.redcircleapi.com/request'
+
+    page = '/s/' + query
+    
+    params = {
+    'api_key': '8AA0D45E8C9142CEA86618549017210A',
+      'search_term': query,
+      'type': 'search'
+    }
+
+    data = requests.get(api_url, params=params).json()
+
+    items = []
+    for p in data['search_results']:
+        if 'price' in p['offers']['primary'] and 'rating' in p['product']:
+            item = {
+                'timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+                'title': formatTitle(p['product']['title']),
+                'price': '$' + str(p['offers']['primary']['price']),
+                'website': 'target',
+                'link': p['product']['link'],
+                'image': p['product']['main_image'],
+                'rating': str(p['product']['rating'])
+            }
+            items.append(item)   
+    return items
+
+
+def scrape_ebay(query):
+    """Scrape Target's api for data
+
+    https://www.countdownapi.com/docs/ebay-product-data-api/overview
+
+    Parameters
+    ----------
+    query: str
+        Item to look for in the api
+
+    Returns
+    ----------
+    items: list
+        List of items from the dict
+    """
+
+    EBAY_APP = 'BradleyE-slash-PRD-2ddd2999f-2ae39cfa'
+
+    try:
+        api = Connection(appid=EBAY_APP, config_file=None, siteid='EBAY-US')
+        response = api.execute('findItemsByKeywords', {'keywords': query})
+    except ConnectionError as e:
+        print(e)
+        return []
+
+    data = response.dict()
+
+    items = []
+    for p in data['searchResult']['item']:
+        item = {
+            'timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+            'title': formatTitle(p['title']),
+            'price': '$' + p['sellingStatus']['currentPrice']['value'],
+            'website': 'ebay',
+            'link': p['viewItemURL'],
+            'image': p['galleryURL'],
+            'rating':'Not Available'
+        }
+        items.append(item)
+
+    return items
+
+def scrape_homedepot(query):
+    """Scrape Target's api for data
+
+    https://app.bigboxapi.com/playground
+    https://api.bigboxapi.com/request?api_key=087D131F8B774985BD8268E1ADBAB6D1&search_term=lawn+mower&type=search
+
+    Parameters
+    ----------
+    query: str
+        Item to look for in the api
+
+    Returns
+    ----------
+    items: list
+        List of items from the dict
+    """
+
+    params = {
+    'api_key': 'E4FF3BE4CDE146E8B9A00C2BC657F953',
+    'search_term': query,
+    'type': 'search'
+    }
+
+    # make the http GET request to BigBox API
+    data = requests.get('https://api.bigboxapi.com/request', params).json()
+    
+    items = []
+    for p in data['search_results']:
+        if 'rating' in p['product']:
+            item = {
+                'timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+                'title': formatTitle(p['product']['title']),
+                'price': '$' + str(p['offers']['primary']['price']),
+                'website': 'homedepot',
+                'link': p['product']['link'],
+                'image': p['product']['primary_image'],
+                'rating':str(p['product']['rating'])
+            }
+
+        items.append(item)
+
+    return items
+
+
+CONFIGS = [COSTCO, BESTBUY]
---
+++ src/scraper/formattr.py
@@ -0,0 +1,120 @@
+"""
+Copyright (C) 2021 SE Slash - All Rights Reserved
+You may use, distribute and modify this code under the
+terms of the MIT license.
+You should have received a copy of the MIT license with
+this file. If not, please write to: secheaper@gmail.com
+
+"""
+from datetime import datetime
+import math
+import html
+
+"""
+The formatter module focuses on processing raw text and returning it in
+the required format.
+"""
+
+
+def formatResult(website, titles, prices, links, images, ratings):
+    """
+    The formatResult function takes the scraped HTML as input, and extracts the
+    necessary values from the HTML code. Ex. extracting a price '$19.99' from
+    a paragraph tag.
+    """
+    
+    title, price, link, image, rating = '', '', '', '', ''
+    if titles:
+        title = titles[0].get_text().strip()
+    if prices:
+        price = prices[0].get_text().strip()
+    if links:
+        link = links[0]['href']
+    if images:
+        try:
+            image = images[0]['src']
+        except KeyError:
+            # Handle the case where 'src' attribute is missing, e.g., set src_image to a default value or log an error.
+            image = "https://images.costco-static.com/ImageDelivery/imageService?profileId=12026540&imageId=1276530-847__1&recipeName=350"
+    if ratings:
+        try:
+            rating = ratings[0].get_text().strip()
+        except KeyError:
+            # Handle the case where 'src' attribute is missing, e.g., set src_image to a default value or log an error.
+            rating = "Not Available"
+    else:
+        rating = "Not Available"
+    product = {
+        'timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+        "title": formatTitle(title),
+        "price": price,
+        "link": f'www.{website}.com{link}',
+        "website": website,
+        "image": image,
+        "rating": rating,
+    }
+    if website == 'costco' or website == 'target':
+        product['link'] = f'{link}'
+    return product
+
+
+def sortList(arr, sortBy, reverse):
+    """
+    The sortList function is used to sort the products list based on the
+    flags provided as args. Currently, it supports sorting by price.
+    """
+    if sortBy == "pr":
+        return sorted(arr, key=lambda x: getNumbers(x["price"]), reverse=reverse)
+    # To-do: sort by rating
+    elif sortBy == "ra":
+        # return sorted(arr, key=lambda x: getNumbers(x.price), reverse=reverse)
+        pass
+    return arr
+
+
+def formatSearchQuery(query):
+    """
+    The formatSearchQuery function formats the search string into a string that
+    can be sent as a url paramenter.
+    """
+    return query.replace(" ", "+")
+
+
+def formatSearchQueryForCostco(query):
+    """
+    The formatSearchQueryForCostco function formats the search string into a string that
+    can be sent as a url paramenter.
+    """
+    queryStrings = query.split(' ')
+    formattedQuery = ""
+    for str in queryStrings:
+        formattedQuery += str
+        formattedQuery += '+'
+    formattedQuery = formattedQuery[:-1]
+    return formattedQuery
+
+
+def formatTitle(title):
+    """
+    The formatTitle function formats titles extracted from the scraped HTML code.
+    """
+    title = html.unescape(title)
+    if(len(title) > 40):
+        return title[:40] + "..."
+    return title
+
+
+def getNumbers(st):
+    """
+    The getNumbers function extracts float values (price) from a string.
+    Ex. it extracts 10.99 from '$10.99' or 'starting at $10.99'
+    """
+    ans = ''
+    for ch in st:
+        if (ch >= '0' and ch <= '9') or ch == '.':
+            ans += ch
+    try:
+        ans = float(ans)
+    except:  # noqa: E722
+        ans = math.inf
+    return ans
