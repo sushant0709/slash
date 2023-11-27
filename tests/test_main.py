ded60359ad729babecc08db8c91c93a152c8334f
Anshul <akhairn@ncsu.edu>
2023-11-27 11:59:06 -0500

    Code cov changes - 5

--- tests/test_main.py
+++ tests/test_main.py
@@ -18,40 +18,7 @@ class TestMain(unittest.TestCase):
         assert getFloatPrice("$123.0$") == 123.0
         assert getFloatPrice("") == 0
     
-    def test_getItemInfoByItemName(self):
-        # building argument
-        item_name = "chair"
-        order_by_col = ""
-        reverse = False
-
-        args = {
-            'search': item_name,
-            'sort': 'pr' if order_by_col == 'price' else 'pr',
-            'des': reverse,
-            'num': 1,
-            'src': ''
-        }
-
-        response = ['amazon', 'walmart', 'target', 'costco', 'bestbuy', 'ebay']
-
-        itemList = getItemInfoByItemName(args)
-        for item in itemList:
-            assert item['website'] in response
-
-    def test_getVarietyCountByWebsite(self):
-        itemList = [
-            {'timestamp': '29/11/2021 11:46:26', 'title': 'HOMEFUN Ergonomic Office Chair, High Bac...', 'price': '$97.98', 'link': 'www.amazon.com/HOMEFUN-Ergonomic-Adjustable-Armrests-Support/dp/B084WWQB5B/ref=sr_1_1?keywords=chair&qid=1638204385&sr=8-1', 'website': 'amazon'}, 
-            {'timestamp': '29/11/2021 11:46:28', 'title': 'Big Joe Milano Bean Bag Chair, Multiple ...', 'price': 'From $48.13', 'link': 'www.walmart.comhttps://wrd.walmart.com/track?pltfm=desktop&tax=4044_103150_97116_91853&rdf=1&plmt=sp-search-middle~desktop~&eventST=click&spQs=apqgqDEG00Gk_QH7Y98s-tvzD2GJBtYRHuyrCs-NV8UWvHysqbYcLsclYVXrTItsxT99dj59OB3kUD_JhC3RyHy-pCq4J6WjLiQcT_qrqQA&pos=1&pgId=chair&bt=1&adUid=ZavTQ7x_M1eGlAtdVKJy6__X2-ZoZz6XZJJn&storeId=1372&mloc=sp-search-middle&pt=search&rd=https%3A%2F%2Fwww.walmart.com%2Fip%2FBig-Joe-Milano-Bean-Bag-Chair-Blue%2F51744431%3Fathbdg%3DL1800', 'website': 'walmart'}, 
-            {'timestamp': '29/11/2021 11:46:29', 'title': 'Steel Folding Chair Black - PDG', 'price': '$11.99', 'website': 'target', 'link': 'https://www.target.com/p/steel-folding-chair-black-pdg/-/A-14777780'}, 
-            {'timestamp': '29/11/2021 11:46:30', 'title': 'Respawn Gaming Chair', 'price': '$229.99', 'link': 'https://www.costco.com/respawn-gaming-chair.product.100388800.html', 'website': 'costco'}, 
-            {'timestamp': '29/11/2021 11:46:35', 'title': 'Razer - Iskur Gaming Chair with Built-in...', 'price': '$349.99', 'link': 'www.bestbuy.com/site/razer-iskur-gaming-chair-with-built-in-lumbar-support-black/6476898.p?skuId=6476898', 'website': 'bestbuy'}, 
-            {'timestamp': '29/11/2021 11:46:36', 'title': 'Steelcase Leap V2 Chair,  -Open Box- Ful...', 'price': '$329.11', 'website': 'ebay', 'link': 'https://www.ebay.com/itm/Steelcase-Leap-V2-Chair-Open-Box-Fully-Loaded-Black-Fabric-/121882349228'}
-            ]
-        answer = {'amazon': 1, 'walmart': 1, 'target': 1, 'costco': 1, 'bestbuy': 1, 'ebay': 1}
-
-        response = getVarietyCountByWebsite(itemList)
-        assert response['amazon'] == answer['amazon'] and response['walmart'] == answer['walmart'] and response['target'] == answer['target'] and response['costco'] == answer['costco'] and response['bestbuy'] == answer['bestbuy'] and response['ebay'] == answer['ebay']
-
+    
     def test_getLowestHighestPriceByWebsite(self):
         itemList = [
             {'timestamp': '29/11/2021 11:46:26', 'title': 'HOMEFUN Ergonomic Office Chair, High Bac...', 'price': '$97.98', 'link': 'www.amazon.com/HOMEFUN-Ergonomic-Adjustable-Armrests-Support/dp/B084WWQB5B/ref=sr_1_1?keywords=chair&qid=1638204385&sr=8-1', 'website': 'amazon'}, 
@@ -67,6 +34,7 @@ class TestMain(unittest.TestCase):
             'walmart': 48.0,
             'target': 11.0,
             'costco': 229.0,
+            'homedepot': 220.0,
             'bestbuy': 349.0,
             'ebay': 329.0
         }
@@ -76,6 +44,7 @@ class TestMain(unittest.TestCase):
             'walmart': "www.walmart.comhttps://wrd.walmart.com/track?pltfm=desktop&tax=4044_103150_97116_91853&rdf=1&plmt=sp-search-middle~desktop~&eventST=click&spQs=apqgqDEG00Gk_QH7Y98s-tvzD2GJBtYRHuyrCs-NV8UWvHysqbYcLsclYVXrTItsxT99dj59OB3kUD_JhC3RyHy-pCq4J6WjLiQcT_qrqQA&pos=1&pgId=chair&bt=1&adUid=ZavTQ7x_M1eGlAtdVKJy6__X2-ZoZz6XZJJn&storeId=1372&mloc=sp-search-middle&pt=search&rd=https%3A%2F%2Fwww.walmart.com%2Fip%2FBig-Joe-Milano-Bean-Bag-Chair-Blue%2F51744431%3Fathbdg%3DL1800",
             'target': "https://www.target.com/p/steel-folding-chair-black-pdg/-/A-14777780",
             'costco': "https://www.costco.com/respawn-gaming-chair.product.100388800.html",
+            'homedepot': "https://www.homedepot.com/b/Furniture-Living-Room-Furniture-Accent-Chairs/Club-Chair/N-5yc1vZcf8gZ1z0jje2?mtc=SEM-BF-HDH-GGL-D59H-059_034_INT_FURN-NA-Multi-NA-DSA-HDH-HDH-NA-NA-NBR-NA-NA-NA-%7C059_034_009_ACCENT_FURNITURE%7C&cm_mmc=SEM-BF-HDH-GGL-D59H-059_034_INT_FURN-NA-Multi-NA-DSA-HDH-HDH-NA-NA-NBR-NA-NA-NA-%7C059_034_009_ACCENT_FURNITURE%7C-71700000081570764-58700006940516739-39700062454164432&gad_source=1&gclid=CjwKCAiAmZGrBhAnEiwAo9qHiVVzNOI5MTZ87gu9PNbLDSpWYNEx1-_5Os3SHefE_vEXGfOO0g6aoxoChyUQAvD_BwE&gclsrc=aw.ds",
             'bestbuy': "www.bestbuy.com/site/razer-iskur-gaming-chair-with-built-in-lumbar-support-black/6476898.p?skuId=6476898",
             'ebay': "https://www.ebay.com/itm/Steelcase-Leap-V2-Chair-Open-Box-Fully-Loaded-Black-Fabric-/121882349228"
             }
@@ -85,6 +54,7 @@ class TestMain(unittest.TestCase):
             'walmart': 48.0,
             'target': 11.0,
             'costco': 229.0,
+            'homedepot': 220.0,
             'bestbuy': 349.0,
             'ebay': 329.0}
 
@@ -93,6 +63,7 @@ class TestMain(unittest.TestCase):
             'walmart': "www.walmart.comhttps://wrd.walmart.com/track?pltfm=desktop&tax=4044_103150_97116_91853&rdf=1&plmt=sp-search-middle~desktop~&eventST=click&spQs=apqgqDEG00Gk_QH7Y98s-tvzD2GJBtYRHuyrCs-NV8UWvHysqbYcLsclYVXrTItsxT99dj59OB3kUD_JhC3RyHy-pCq4J6WjLiQcT_qrqQA&pos=1&pgId=chair&bt=1&adUid=ZavTQ7x_M1eGlAtdVKJy6__X2-ZoZz6XZJJn&storeId=1372&mloc=sp-search-middle&pt=search&rd=https%3A%2F%2Fwww.walmart.com%2Fip%2FBig-Joe-Milano-Bean-Bag-Chair-Blue%2F51744431%3Fathbdg%3DL1800",
             'target': "https://www.target.com/p/steel-folding-chair-black-pdg/-/A-14777780",
             'costco': "https://www.costco.com/respawn-gaming-chair.product.100388800.html",
+            'homedepot': "https://www.homedepot.com/b/Furniture-Living-Room-Furniture-Accent-Chairs/Club-Chair/N-5yc1vZcf8gZ1z0jje2?mtc=SEM-BF-HDH-GGL-D59H-059_034_INT_FURN-NA-Multi-NA-DSA-HDH-HDH-NA-NA-NBR-NA-NA-NA-%7C059_034_009_ACCENT_FURNITURE%7C&cm_mmc=SEM-BF-HDH-GGL-D59H-059_034_INT_FURN-NA-Multi-NA-DSA-HDH-HDH-NA-NA-NBR-NA-NA-NA-%7C059_034_009_ACCENT_FURNITURE%7C-71700000081570764-58700006940516739-39700062454164432&gad_source=1&gclid=CjwKCAiAmZGrBhAnEiwAo9qHiVVzNOI5MTZ87gu9PNbLDSpWYNEx1-_5Os3SHefE_vEXGfOO0g6aoxoChyUQAvD_BwE&gclsrc=aw.ds",,
             'bestbuy': "www.bestbuy.com/site/razer-iskur-gaming-chair-with-built-in-lumbar-support-black/6476898.p?skuId=6476898",
             'ebay': "https://www.ebay.com/itm/Steelcase-Leap-V2-Chair-Open-Box-Fully-Loaded-Black-Fabric-/121882349228"}
 
