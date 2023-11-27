a96eca96d721bc574aab76182097c85ca79ff157
Parth Kulkarni <91055071+ParthK7@users.noreply.github.com>
2023-11-27 10:34:55 -0500

    lib folder added

---
+++ lib/SearchPage.dart
@@ -0,0 +1,136 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:slash_app/ResultPage.dart';
+import 'package:slash_app/GraphPage.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  List<DropdownMenuItem<String>> get dropdownItems {
+    List<DropdownMenuItem<String>> menuItems = [
+      const DropdownMenuItem(child: Text("CostCo"), value: "costco"),
+      const DropdownMenuItem(child: Text("Ebay"), value: "ebay"),
+      const DropdownMenuItem(child: Text("Walmart"), value: "walmart"),
+    ];
+    return menuItems;
+  }
+
+  String selectedValue = "walmart";
+  IconData selectedIcon = Icons.search_rounded;
+  final _textController = TextEditingController();
+
+  void _navigateToResultScreen() {
+    String searchText = _textController.text;
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ResultPage(selectedValue, searchText),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final Widget pageBody = Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          DropdownButton(
+            alignment: AlignmentDirectional.center,
+            borderRadius: BorderRadius.circular(15),
+            elevation: 10,
+            iconEnabledColor: Colors.black,
+            padding: const EdgeInsets.all(10),
+            items: dropdownItems,
+            value: selectedValue,
+            onChanged: (String? newValue) {
+              setState(() {
+                selectedValue = newValue!;
+              });
+            },
+          ),
+          SizedBox(height: 20),
+          TextField(
+            controller: _textController,
+            textCapitalization: TextCapitalization.sentences,
+            autocorrect: true,
+            cursorColor: Colors.black,
+            showCursor: true,
+            onTap: () => setState(() {
+              selectedIcon = Icons.clear_rounded;
+            }),
+            decoration: InputDecoration(
+              hintText: 'Search Products...',
+              hintStyle: TextStyle(color: Colors.black),
+              suffixIcon: IconButton(
+                onPressed: () {
+                  _textController.clear();
+                },
+                icon: Icon(selectedIcon),
+              ),
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.all(15),
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            height: mediaQuery.size.height / 15,
+            width: mediaQuery.size.width / 5,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: TextButton(
+              onPressed: _navigateToResultScreen,
+              child: const Text(
+                'Search',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            navigationBar: const CupertinoNavigationBar(
+              middle: Text('Slash App'),
+            ),
+            child: pageBody,
+          )
+        : Scaffold(
+            appBar: AppBar(
+              title: const Text('Slash App'),
+              backgroundColor: Theme.of(context).primaryColor,
+              foregroundColor: Colors.black,
+            ),
+            drawer: Drawer(
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text('Search Product'),
+                    onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context)=> SearchPage())),
+                  ),
+                  ListTile(
+                    title: Text('Graphs'),
+                    onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context)=> GraphPage())),
+                  ),
+                ],
+              ),
+            ),
+            body: pageBody,
+          );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:slash_app/SearchPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Slash App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrangeAccent),
+        useMaterial3: true,
+      ),
+      home: SearchPage(),
+    );
+  }
+}
